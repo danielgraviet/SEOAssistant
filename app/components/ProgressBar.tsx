2eed049d8c813c65698f7dac5629117e3521bed3
danielgraviet <dgravs5@gmail.com>
2024-11-20 16:17:06 -0700

    first commit

---
+++ app/components/ProgressBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <ul className="steps">
+      <li className="step step-primary">Key Phrase</li>
+      <li className="step">Add Content</li>
+      <li className="step">Choose Images</li>
+      <li className="step">Finish</li>
+      
+    </ul>
+  );
+};
+
+export default ProgressBar;
