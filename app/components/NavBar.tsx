a7d1e1a74228cdedcb6d7af26d093ca15201b300
danielgraviet <dgravs5@gmail.com>
2024-12-06 18:32:22 -0700

    added profile logo, and implemented a basic login function

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    console.log("User logged in!");
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    console.log("User logged out!");
+  };
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -22,32 +35,37 @@ const NavBar = () => {
           >
             <div className="w-10 rounded-full">
               <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                alt="Profile"
+                src="/dtg.png"
+                className="w-10 rounded-full"
               />
             </div>
           </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
+          > {isLoggedIn ? (
+            <>
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </>
+          ) : (
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <a onClick={handleLogin}>Login</a>
             </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+          )}
+        </ul>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default NavBar;
